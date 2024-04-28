8dd5973b3aa65a683e5b11c0c8cb1966956cc700
MaciejBascik <96304157+MaciejBascik@users.noreply.github.com>
2024-04-28 15:28:48 +0200

    deployment fixes

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -24,51 +24,39 @@ export default function Home() {
       }
     }
   return (
-
-<div>
+    <div>
       <div className="hero min-h-screen" style={{backgroundImage: 'url(https://i.ibb.co/jk0X5z9/Group-6.png)'}}>
         <div className="text-left text-neutral-content max-w-[100%]">
-        
-
-        <div className="relative flex flex-col justify-center h-screen overflow-hidden ">
-        <div className="w-full p-12 m-auto bg-white rounded-md shadow-md lg:max-w-lg ">
-            <h1 className="text-2xl font-semibold text-center text-[#001354] pb-5">Log in to continue</h1>
-            <form className="space-y-6" onSubmit={handleSubmit} >
+          <div className="relative flex flex-col justify-center h-screen overflow-hidden ">
+            <div className="w-full p-12 m-auto bg-white rounded-md shadow-md lg:max-w-lg ">
+              <h1 className="text-2xl font-semibold text-center text-[#001354] pb-5">Log in to continue</h1>
+              <form className="space-y-6" onSubmit={handleSubmit} >
                 <div>
-                    <label className="label">
-                        <span className="text-base label-text">Email</span>
-                    </label>
-                    <input type="text" placeholder="Email Address" className="w-full input input-bordered input-primary bg-white" required />
+                  <label className="label">
+                    <span className="text-base label-text">Email</span>
+                  </label>
+                  {/* Użycie inputa z ustawieniem typu 'email' */}
+                  <input type="email" placeholder="Email Address" className="w-full input input-bordered input-primary bg-white" required />
                 </div>
                 <div>
-                    <label className="label">
-                        <span className="text-base label-text">Password</span>
-                    </label>
-                    <input type="password" placeholder="Enter Password"
-                        className="w-full input input-bordered input-primary bg-white" required />
+                  <label className="label">
+                    <span className="text-base label-text">Password</span>
+                  </label>
+                  {/* Użycie inputa z ustawieniem typu 'password' */}
+                  <input type="password" placeholder="Enter Password" className="w-full input input-bordered input-primary bg-white" required />
                 </div>
                 <div >
-                <a href="#" className="text-xs text-gray-600 hover:underline hover:text-blue-600 ">Forget Password?</a><br/>
-                <a href="#" className="text-xs text-gray-600 hover:underline hover:text-blue-600">You don't have account? Sign-up</a>
+                  <a className="text-xs text-gray-600 hover:underline hover:text-blue-600">Forget Password?</a><br/>
+                  <a className="text-xs text-gray-600 hover:underline hover:text-blue-600">You don't have account? Sign-up</a>
                 </div>
                 <div>
-                    <button className="btn bg-gradient-to-r from-[#000000] to-[#001354] text-white">Log In</button>
+                  <button className="btn bg-gradient-to-r from-[#000000] to-[#001354] text-white">Log In</button>
                 </div>
-            </form>
-        </div>
-
-
-        </div>
-    
-
-
+              </form>
+            </div>
+          </div>
         </div>
       </div>
-
-
-
-
-
-</div>
+    </div>
   );
 }
