749f5fd185fd0e70fb528b5d91f2992fc087d58c
MaciejBascik <96304157+MaciejBascik@users.noreply.github.com>
2024-05-17 16:19:48 +0200

    fixing cors issue

--- src/app/api/token.js
+++ src/app/api/token.js
@@ -7,7 +7,7 @@ export default async function getUser(code) {
     body: {
         'client_key': 'aw56hlbjs9cydo18',
         'client_secret': '8ITb87gRwCOFSURt5FgECyhYqT0OCfXw',
-        'code': 'DXCLNE0j0KQ9mSwdDv7BsUr12T4UnasJ60GWAx3FXXG9wEARkidCyx7jZyN3n_2LXlUSCFLykIzT-t_IPzRBMe0m2w7hwoEMHVrTy05xFafoVwvqkcty9vScswzl76_jufCurfDg65ZEaCrXWCZYKJ23PBqJVy-lYdZs2oFErWfVabV8zRfAg5L1VUCGVyfj4-_s2x6Zt-C9fPIu2N0IHw*2!5934.e1',
+        'code': code,
         'grant_type': 'authorization_code',
         'redirect_uri': 'https://viralclothes.vercel.app/user/feed/'
     },
